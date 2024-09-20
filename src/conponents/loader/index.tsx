bc4885bfede913a3e78c3c5649b692f639ffec57
Adham Khalifa <adhamkhalifa94@gmail.com>
2024-09-20 22:46:11 +0300

    add logo webp

--- src/conponents/loader/index.tsx
+++ src/conponents/loader/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./style.module.scss";
 import Image from "next/image";
-import logo from "../../assets/logo-removebg-preview_prev_ui.png";
+import logo from "../../assets/logo-removebg-preview_prev_ui.webp";
 const Loader = () => {
   return (
     <div className={styles.loaderContainer}>
